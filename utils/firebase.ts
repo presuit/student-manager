14d2d38c6883d94ccd87596cda798842b6e66a87
presuit <chldudgh8256@naver.com>
2022-10-02 19:34:23 +0900

    feature:initial setting

---
+++ utils/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsnyFFxywldPIylLaTSfZ4PrOJLQ22i5M",
+  authDomain: "students-manager-13336.firebaseapp.com",
+  projectId: "students-manager-13336",
+  storageBucket: "students-manager-13336.appspot.com",
+  messagingSenderId: "784186781356",
+  appId: "1:784186781356:web:85c0eb117a0e43301bf909",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
